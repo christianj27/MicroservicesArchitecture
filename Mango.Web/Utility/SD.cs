170607a2ba9536b16e8c2d5b503c0773c60d7139
Christian J <christian.jeremia@caditglobal.com>
2025-07-02 12:25:01 +0700

    Add project files.

---
+++ Mango.Web/Utility/SD.cs
@@ -0,0 +1,11 @@
+﻿namespace Mango.Web.Utility
+{
+    public class SD //Static Details
+    {
+        public static string CouponAPIBase {  get; set; }
+        public enum ApiType
+        {
+            GET, POST, PUT, DELETE
+        }
+    }
+}
